7fd4f0ca3c4934ef6cb485e422ebd7fd261bb4e5
Andrés Jaime Eduardo Díaz Mercado <777@alumnos.fpsumma.es>
2026-02-11 13:30:30 +0100

    nuevo

--- src/main/java/es/fplumara/dam1/textapp/config/AppConfig.java
+++ src/main/java/es/fplumara/dam1/textapp/config/AppConfig.java
@@ -1,4 +1,46 @@
 package es.fplumara.dam1.textapp.config;
 
+import es.fplumara.dam1.textapp.exceptions.ConfigException;
+import es.fplumara.dam1.textapp.files.StoreType;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Properties;
+
 public class AppConfig {
+
+    private StoreType storeType;
+    private String messagesFile;
+    private int maxLenght;
+
+
+    public File getStoreType() {
+
+        return File;
+    }
+
+    public int getMaxLenght() {
+        return maxLenght;
+    }
+
+    public String getMessagesFile() {
+
+        return messagesFile;
+    }
+
+    public AppConfig() {
+        Path path = Path.of("C:\\Users\\AndrésJaimeEduardoDí\\OneDrive - SUMMA Formación Profesional\\Documentos\\GitHub\\Mini-aplicacion\\data\\config.properties");
+        Properties properties = new Properties();
+
+        try (InputStream in = Files.newInputStream(path)) {
+            properties.load(in);
+        } catch (IOException e) {
+            throw new ConfigException("");
+        }
+
+
+    }
 }
